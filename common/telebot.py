2c4392734d8455512b4e8971d2d92934e45bbdab
vas3k <me@vas3k.ru>
2016-11-23 19:15:36 +0200

    New telebot

--- common/telebot.py
+++ common/telebot.py
@@ -5,11 +5,7 @@ from settings import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, TELEGRAM_CHANNEL_ID
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
-def post_new_story(text, url, disable_web_page_preview=False):
-    if disable_web_page_preview:
-        full_text = "{} {}".format(text, url)
-    else:
-        full_text = "{}\n\n{}".format(text, url)
-
-    bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=full_text, disable_web_page_preview=disable_web_page_preview)
-    bot.sendMessage(chat_id=TELEGRAM_CHANNEL_ID, text=full_text, disable_web_page_preview=disable_web_page_preview)
+def post_new_story(text, with_chat=True):
+    if with_chat:
+        bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=text, disable_web_page_preview=False)
+    bot.sendMessage(chat_id=TELEGRAM_CHANNEL_ID, text=text, disable_web_page_preview=False)
--- models/vas3k_365.py
+++ models/vas3k_365.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
 from datetime import datetime
 
+import re
 import requests
-from flask import render_template, request
+from flask import render_template, request, Markup
 
 from base.model import BaseAdminModel
 from base.view import BaseView
 from common.telebot import post_new_story
-from db.vas3kru import Vas3kDatabase, Story
+from db.vas3kru import Vas3kDatabase, Story, Comment
 
 
 class The365AdminModel(BaseAdminModel):
@@ -29,6 +31,8 @@ class The365AdminModel(BaseAdminModel):
             if file.filename == "":
                 return render_template("error.html", message="No file")
 
+            announce_text = request.form.get("text") or "В эфире программа «дратути». Коротко за сегодня:"
+
             saved_filename = "/tmp/365.{}".format(file.filename[file.filename.rfind(".") + 1:])
             file.save(saved_filename)
 
@@ -41,7 +45,13 @@ class The365AdminModel(BaseAdminModel):
 
             today_date = datetime.now().strftime("%Y-%m-%d")
 
-            self.model.create(
+            previous_story = self.model.session.\
+                query(Story).\
+                filter(Story.type == '365').\
+                order_by(Story.created_at.desc()).\
+                first()
+
+            new_story = self.model.create(
                 slug=today_date,
                 type="365",
                 title=today_date,
@@ -57,8 +67,46 @@ class The365AdminModel(BaseAdminModel):
                 is_sexy_title=False
             )
 
-            post_new_story(text="365 project: фотка каждый день", url="http://vas3k.ru/365/{}/".format(today_date))
+            telegram_text = "{}\n\n365 project: http://vas3k.ru/365/{}/".format(announce_text, new_story.slug)
+
+            daily_comments = self.model.session.\
+                query(Comment).\
+                join(Story, Story.id == Comment.story_id).\
+                filter(Comment.created_at > previous_story.created_at).\
+                order_by(Comment.block)
+
+            group_comments = defaultdict(list)
+
+            for comment in daily_comments:
+                group_comments[comment.story].append(comment)
+
+            for story, comments in group_comments.items():
+                telegram_text += "\n\nКомментарии к «{}»:".format(story.title)
+                for comment in comments:
+                    telegram_text += "\n"
+                    telegram_text += self.make_comment_text(comment)
+
+            post_new_story(text=telegram_text, with_chat=False)
 
             return render_template("success.html", message="Saved: <a href='http://vas3k.ru/365/{today}/'>http://vas3k.ru/365/{today}/</a>".format(today=today_date))
+
+        def make_comment_text(self, comment):
+            text = Markup(comment.text).striptags()
+            text = re.sub(r'^https?:\/\/.*[\r\n]*', '[URL]', text, flags=re.MULTILINE)
+            if comment.block:
+                link = "http://vas3k.ru/{type}/{slug}/#block-{block}-{id}".format(
+                    type=comment.story.type,
+                    slug=comment.story.slug,
+                    block=comment.block,
+                    id=comment.id
+                )
+            else:
+                link = "http://vas3k.ru/{type}/{slug}/#comment{id}".format(
+                    type=comment.story.type,
+                    slug=comment.story.slug,
+                    id=comment.id
+                )
+
+            return " {}: {} {}".format(comment.author, text, link)
 
     list_view = IndexView
