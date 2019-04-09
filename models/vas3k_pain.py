9a74c87cc7b32e4c5c9f12707ddf0176edbea108
vas3k <me@vas3k.ru>
2019-04-09 21:25:30 +0200

    Rename solution -> solutions

--- models/vas3k_pain.py
+++ models/vas3k_pain.py
@@ -16,7 +16,7 @@ class PainAdminModel(BaseAdminModel):
     ordering = Pain.created_at.desc()
     widgets = {
         "problem": LongTextWidget,
-        "solution": LongTextWidget
+        "solutions": LongTextWidget
     }
 
     class PainListView(BaseListView):
