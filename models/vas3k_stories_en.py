ebcbe2b62e2cdb238065152312effc8789340d76
vas3k <me@vas3k.ru>
2020-03-01 14:32:58 +0100

    Fix cache

--- models/vas3k_stories_en.py
+++ models/vas3k_stories_en.py
@@ -71,7 +71,7 @@ class StoryENAdminModel(BaseAdminModel):
     def after_update(self, old_item, new_item):
         if old_item.text != new_item.text:
             self.session.execute(
-                "update stories set text_cache = '', text_cache_rss = '' where id = :story_id",
+                "update stories_en set text_cache = '', text_cache_rss = '' where id = :story_id",
                 {"story_id": new_item.id}
             )
             self.session.commit()
